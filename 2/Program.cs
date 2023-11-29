b5a50baf4008249dc85d63a2537296e1a5973b99
elena <princessa-len4ik@mail.ru>
2023-11-29 17:50:31 +0500

    Решение 2 задачи

---
+++ 2/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(1, 100); 
+Console.WriteLine(string.Join(", ", array));
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+if (array[i] % 2 == 0)
+count++;
+Console.WriteLine("Количество элементов четных = " + count);
