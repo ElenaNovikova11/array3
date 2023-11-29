97984b8706a6563f9262fb2a3daaf1a090bce775
elena <princessa-len4ik@mail.ru>
2023-11-29 17:50:06 +0500

    Решение 1 задачи

---
+++ 1/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(1, 100); 
+Console.WriteLine(string.Join(", ", array));
+int A = 20; 
+int B = 90;
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+if ((array[i] >= A) && (array[i] <= B))
+count++;
+Console.WriteLine("Количество элементов в диапазоне от 20 до 90 = " + count);
+
